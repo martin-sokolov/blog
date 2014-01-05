19a45a688df0cd201cbd748df2c2b368c6cfbe77
martin-sokolov <apple.it00@gmail.com>
2014-01-05 21:34:05 +0200

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  # POST /comments
+  # POST /comments.json
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(params[:comment])
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { redirect_to @comment.article }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @comment.article }
+      format.json { head :no_content }
+    end
+  end
+end
